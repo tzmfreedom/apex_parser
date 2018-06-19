0708ad2de4c1b8f0c20df2191b5e366a61544f1c
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2018-06-19 09:57:44 +0900

    Add tokens

--- lib/apex.rex.rb
+++ lib/apex.rex.rb
@@ -1,27 +1,67 @@
 class ApexCompiler
 macro
   BLANK  [\ \t]+
+  REMIN \/\*
+  REMOUT \*\/
 rule
-  {BLANK}
-  \{        { [:LC_BRACE, text] }
-  \}        { [:RC_BRACE, text] }
-  \(        { [:L_BRACE, text] }
-  \)        { [:R_BRACE, text] }
-  '[\s\w]*' { [:STRING, text[1..-2]] }
-  class     { [:CLASS, text] }
-  public    { [:PUBLIC, text] }
-  return    { [:RETURN, text] }
-  \d+       { [:INTEGER, text.to_i] }
-  [A-Z]\w*       { [:U_IDENT, text] }
-  [a-z]\w*       { [:IDENT, text] }
-  \n
-  \+         { [:ADD, text] }
-  \-         { [:SUB, text] }
-  \*         { [:MUL, text] }
-  \/         { [:DIV, text] }
-  =         { [:ASSIGN, text] }
-  ;         { [:SEMICOLON, text]}
-  ,         { [:COMMA, text] }
-  \.         { [:DOT, text] }
-  .         { [text, text] }
+      {BLANK}
+      {REMIN}          { state = :REM; [:REM_IN, text] }
+:REM  {REMOUT}         { state = nil; [:REM_OUT, text] }
+:REM  (.+)(?={REMOUT}) { [:COMMENT, text] }
+      \/\/.*\n         { [:COMMENT, text] }
+      \{               { [:LC_BRACE, text] }
+      \}               { [:RC_BRACE, text] }
+      \(               { [:L_BRACE, text] }
+      \)               { [:R_BRACE, text] }
+      '[\s\w]*'        { [:STRING, text[1..-2]] }
+      @\w+             { [:ANNOTATION, text] }
+      insert           { [:INSERT, text] }
+      delete           { [:DELETE, text] }
+      undelete         { [:UNDELETE, text] }
+      update           { [:UPDATE, text] }
+      upsert           { [:UPSERT, text] }
+      before           { [:BEFORE, text] }
+      after            { [:AFTER, text] }
+      trigger          { [:TRIGGER, text] }
+      on               { [:ON, text] }
+      with             { [:WITH, text] }
+      without          { [:WITHOUT, text] }
+      sharing          { [:SHARING, text] }
+      class            { [:CLASS, text] }
+      public           { [:PUBLIC, text] }
+      private          { [:PRIVATE, text] }
+      protected        { [:PROTECTED, text] }
+      global           { [:GLOBAL, text] }
+      override         { [:OVERRIDE, text] }
+      global           { [:GLOBAL, text] }
+      static           { [:STATIC, text] }
+      final            { [:FINAL, text] }
+      new              { [:NEW, text] }
+      get              { [:GET, text] }
+      set              { [:SET, text] }
+      extends          { [:EXTENDS, text] }
+      implements       { [:IMPLEMENTS, text] }
+      abstract         { [:ABSTRACT, text] }
+      virtual          { [:VIRTUAL, text] }
+      instance_of      { [:INSTANCE_OF, text] }
+      return           { [:RETURN, text] }
+      \d+              { [:INTEGER, text.to_i] }
+      [A-Z]\w*         { [:U_IDENT, text] }
+      [a-z]\w*         { [:IDENT, text] }
+      \n
+      \+               { [:ADD, text] }
+      \-               { [:SUB, text] }
+      \*               { [:MUL, text] }
+      \/               { [:DIV, text] }
+      ==               { [:EQUAL, text] }
+      !=               { [:NOT_EQUAL, text] }
+      <                { [:LESS_THAN, text] }
+      >                { [:GREATER_THAN, text] }
+      <=               { [:LESS_THAN_EQUAL, text] }
+      >=               { [:GREATER_THAN_EQUAL, text] }
+      =                { [:ASSIGN, text] }
+      ;                { [:SEMICOLON, text]}
+      ,                { [:COMMA, text] }
+      \.               { [:DOT, text] }
+      .                { [text, text] }
 end
